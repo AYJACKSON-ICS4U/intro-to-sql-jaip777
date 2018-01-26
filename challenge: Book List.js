389204a3e1045c13f68e7050085c6a3cfac3c429
Jai <31632266+jaip777@users.noreply.github.com>
2018-01-26 10:12:51 -0500

    Create challenge: Book List.js

---
+++ challenge: Book List.js
@@ -0,0 +1,6 @@
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER );
+
+INSERT INTO books VALUES (1, "LOTR", 4);
+INSERT INTO books VALUES (2, "Fablehaven", 1);
+INSERT INTO books VALUES (3, "The Hobbit", 2);
+SELECT * FROM books;
